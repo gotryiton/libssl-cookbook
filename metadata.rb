e6b4c434094411341a157f1d5fe06d1b8cc75536
Scott Bader <sb@scottbader.org>
2013-04-18 14:32:28 -0400

    adding metadata

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "gotryiton"
+maintainer_email "scott@gotryiton.com"
+license          "Apache 2.0"
+description      "Installs libssl"
+long_description "Installs libssl"
+version          "0.1.0"
+
